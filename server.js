96ba753a03221fbd07fff3a1ea9204f96b9c6d80
Keith <kbarber34@ivytech.edu>
2024-02-08 16:57:10 -0500

    sending multiple html pages

--- server.js
+++ server.js
@@ -1,7 +1,35 @@
 const http = require("http");
+const fs = require("fs");
 
 const server = http.createServer((request, response) => {
-    console.log("request made");
+    console.log(request.url, request.method);
+
+    //set header content type
+    response.setHeader("Content-Type", "text/html");
+    
+    let path = "./views/";
+    switch(request.url){
+        case "/":
+            path += "index.html";
+            break;
+        case "/about":
+            path += "about.html";
+            break;
+        default:
+            path += "404.html";
+            break;
+    }
+
+    //send html file
+    fs.readFile(path, (err, data) => {
+        if (err){
+            console.log(err);
+            response.end();
+        }
+        else{
+            response.end(data);
+        }
+    });
 });
 
 server.listen(3000, "localhost", () => {
